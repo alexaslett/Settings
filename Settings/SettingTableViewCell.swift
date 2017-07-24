b2b0a4679736d384e82305cb9e436fe145f2cce7
Alex Aslett <alexaslett@gmail.com>
2017-07-24 12:20:20 -0600

    Implement SettingsViewController and SettingTableViewCell

---
+++ Settings/SettingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingTableViewCell.swift
+//  Settings
+//
+//  Created by Alex Aslett on 7/24/17.
+//  Copyright © 2017 One Round Technology. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
